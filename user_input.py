3d1ef11ac5a638b4f6feda5b7dd693df04acf56b
alloo4 <darylopiyo7603@gmail.com>
2024-10-28 19:04:13 +0300

    Add user_input.py script for personalized message

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# user_input.py
+
+# My name, age, and location
+name = input("Daryl Alloo: ")
+age = input("23: ")
+location = input("Kileleshwa: ")
+
+# personalized message
+print(f"Hello {Daryl}, you are {23} years old and live in {Kileleshwa}.")
